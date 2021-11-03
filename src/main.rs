2541216cbed8d9031e7b5aafe559109e48891709
Koki Honda <kokihonda@am.ics.keio.ac.jp>
2021-11-03 14:15:51 +0900

    設定ファイルの追加

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use manage::startup::run;
+use manage::configuration::get_configuration;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> hyper::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).unwrap();
     run(listener)?.await
 }
