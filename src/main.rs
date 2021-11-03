346c6f9c4922b89717eb9f3e337d01d01105441c
Koki Honda <kokihonda@am.ics.keio.ac.jp>
2021-11-03 13:55:30 +0900

    test追加

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use axum::{routing::get, Router};
+use manage::run;
+use std::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
-
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+async fn main() -> hyper::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+    run(listener)?.await
 }
